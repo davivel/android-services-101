5fb241bdc44d8a2d4b061494ea8a59c0a05fc285
David A. Velasco <dvelasco@solidgear.es>
2017-05-18 07:52:40 +0200

    Add StartedService project, as created with Android Studio wizard, with blank Activity

---
+++ StartedService/app/src/main/java/net/dvelasco/android/startedservice/TheActivity.java
@@ -0,0 +1,13 @@
+package net.dvelasco.android.startedservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the);
+    }
+}
